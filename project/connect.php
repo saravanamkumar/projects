f25be27cbeb5514a9991d253ddfc5202e258c374
saravana kumar <saravanasarao72@gmail.com>
2017-04-04 10:19:34 +0530

    initial commit

---
+++ project/connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername="saravana";
+$username="root";
+$password="password";
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
